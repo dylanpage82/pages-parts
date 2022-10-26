19390970b8d64559d643112cf0902305424aa055
Dylan <dylanpage82@gmail.com>
2022-10-25 19:21:00 -0500

    edit route complete

--- views/parts/Edit.jsx
+++ views/parts/Edit.jsx
@@ -3,7 +3,18 @@ const Default = require('../layouts/Default.jsx')
 
 class Edit extends React.Component {
   render () {
-    
+    const { name, manufacture, description, _id, price } = this.props.part
+    return(
+      <Default>
+        <form method='POST' action={`/parts/${_id}?_method=PUT`}>
+          Name: <input type='text' name='name' defaultValue={name} /><br />
+          Manufacure: <input type='text' name='manufacture' defaultValue={manufacture} /><br />
+          Description: <input type='text' name='description' defaultValue={description} /><br />
+          Price: <input type='number' name='price' defaultValue={price} />
+          <input type='submit' value='Submit Part' />
+        </form>
+      </Default>
+    )
   }
 }
 
